7e32d85ebb0ca8e365b0cd0bcbd49c3f61a05c9e
Omaryasir <omaryasirm@gmail.com>
2024-03-24 16:08:53 +0500

    Add core pages

---
+++ app/NavBar.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+import { Container, Flex } from "@radix-ui/themes";
+
+const NavBar = () => {
+  const currentPath = usePathname();
+
+  const links = [
+    { label: "Forum", href: "/forum" },
+    { label: "Admin", href: "/admin" },
+  ];
+
+  return (
+    <nav className="border-b mb-5 px-5 h-14 flex items-center">
+      <Container>
+        <Flex justify={"between"}>
+          <Flex align={"center"} gap={"3"}>
+            <ul className="flex space-x-6">
+              {links.map((link) => (
+                <li key={link.href}>
+                  <Link
+                    className={`${
+                      link.href === currentPath && "text-zinc-900"
+                    } text-zinc-500 hover:text-zinc-800 transition-colors`}
+                    href={link.href}
+                  >
+                    {link.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </Flex>
+        </Flex>
+      </Container>
+    </nav>
+  );
+};
+
+export default NavBar;
